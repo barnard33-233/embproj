baf49c77ed2f7984c68825360e8444f112c277a7
Qing_LKYi <597572246@qq.com>
2023-05-30 02:16:28 +0800

    refactor: change function name and feed iwdg more

--- Src/main.c
+++ Src/main.c
@@ -90,7 +90,7 @@ void print_data(void);
 void HAL_SYSTICK_Callback(void);
 void init_device(int);
 void loop_delay(int time);
-void init_buttons();
+void init_keyboard(void);
 //__STATIC_INLINE void disable_SysTick(void); 
 //__STATIC_INLINE void enable_SysTick(void);
 /* Private function prototypes -----------------------------------------------*/
@@ -100,60 +100,66 @@ int main(void)
 
   /* MCU Configuration */
   int hot = restore_data();
-  // wait 100ms when cold boot
+  // delay when cold boot
   if (hot == 0) loop_delay(1000);
 
   init_device(hot);
 
+  IWDG_Feed();
   if (hot == 1) printf("A hot booting... \r\n");
   printf("-------------------------------------------------\r\n");
   printf(" Muti speed music player! \r\n");
   printf("-------------------------------------------------\r\n");
+  IWDG_Feed();
   print_data();
 	printf("-------------------------------------------------\r\n");
-  IWDG_Feed();
 
   /* Infinite loop */
   while (1)
   {
+    IWDG_Feed();
     if (get_stop() == 1) enable_music = 0;
     else enable_music = 1;
-    if (flush_timer /*get_flush_timer()*/ >= 500000) {
-      // 定时事件 每 500 ms
+    if (flush_timer /*get_flush_timer()*/ >= 80000) {
+      // 定时事件 每 80 ms
       flush_timer = 0; // reset_flush_timer();
       // 同步所有备份数据
+      IWDG_Feed();
       recover_backups();
       IWDG_Feed();
+      init_beep();
     }
 		if (get_flag1() >= 1) {
-      if(get_flag1() >= MAX_FLAG1){
-        init_buttons();
+      if(get_flag1() >= MAX_FLAG1) { // 输入滤波
+        IWDG_Feed();
+        init_keyboard();
       }
       uint8_t tmp_rx1 = 0;
 			set_flag1(0);
       comp_flag = 0; // 序列完整性标志
       IWDG_Feed();
 			I2C_ZLG7290_Read(&hi2c1, 0x71, 0x01, &tmp_rx1, 1);
-      comp_flag ++;
       set_Rx1_Buffer(tmp_rx1);
-      comp_flag ++;
+      comp_flag |= 1;
       IWDG_Feed();
-
 			switch_key(); // 更新 flag 的值
-      // random delay
+      comp_flag |= 2;
+      // 在输入与处理间添加随机时延
       loop_delay(rand() % 100);
-      comp_flag ++;
       if (comp_flag == 3) { 
-        printf("Get keyvalue = %#x => flag = %d\r\n", get_Rx1_Buffer(), get_flag());
-        switch_flag();
         IWDG_Feed();
+        printf("Get keyvalue = %#x => flag = %d\r\n", get_Rx1_Buffer(), get_flag());
+        IWDG_Feed();
+        switch_flag();
       } else {
         // 不等于 3 意味着攻击者跳过了前面的语句
+        IWDG_Feed();
         printf("You may under an attack. Input Abort.\r\n");
       }
 		}
     if (enable_music == 0) continue;
-    // uint32_t music_timer = get_music_timer();
+    // 音符计算模块
+    IWDG_Feed();
 		uint32_t score_index = get_score_index();
 		if(music_timer >= note_time - note_time/32){
       present_pitch = pause;
@@ -163,24 +169,26 @@ int main(void)
       present_pitch = score[score_index].pitch;
       uint16_t tmp_speed = get_speed();
       if (tmp_speed < 50 || tmp_speed > 170) {
+        // 速度复原
+        IWDG_Feed();
         printf("Bad speed value. You may under an attack.\r\n");
         printf("Reset speed to 120.\r\n");
         set_speed(120);
-        IWDG_Feed();
       }
+      // 写入音符信息
+      IWDG_Feed();
       note_time = Du_to_us(present_du);
       set_score_index((score_index + 1) % SCORE_LENGTH);
       music_timer = 0;
     }
-    IWDG_Feed();
 		if(present_pitch != pause){
+      IWDG_Feed();
 			HAL_GPIO_WritePin(GPIOG,GPIO_PIN_6,GPIO_PIN_SET);
 			HAL_Delay(present_pitch);
       IWDG_Feed();
 			HAL_GPIO_WritePin(GPIOG,GPIO_PIN_6,GPIO_PIN_RESET);
 			HAL_Delay(present_pitch);
 		}
-    IWDG_Feed();
   }
 }
 
@@ -233,21 +241,18 @@ void init_device(int hot) {
   IWDG_Init();
 }
 
-void init_buttons(){
-  __GPIOD_CLK_ENABLE(); // 按键
-  __GPIOB_CLK_ENABLE(); // i2c
-
-  /*Configure GPIO pin : PD13 */ // button
-  GPIO_InitStruct.Pin = GPIO_PIN_13;
-  GPIO_InitStruct.Mode = GPIO_MODE_IT_FALLING;
-  GPIO_InitStruct.Pull = GPIO_NOPULL;
-  HAL_GPIO_Init(GPIOD, &GPIO_InitStruct);
-
-  /* EXTI interrupt init */ // button
-  HAL_NVIC_SetPriority(EXTI15_10_IRQn, 0, 0);
-  HAL_NVIC_EnableIRQ(EXTI15_10_IRQn);
-
+void init_keyboard(void) {
+  GPIO_Init_Keyboard();
   MX_I2C1_Init();
+}
+
+void init_beep(void) {
+  GPIO_Init_Beep();
+}
+
+void init_uart(void) {
+  __GPIOA_CLK_ENABLE();
+  MX_USART1_UART_Init();
 }
 
 /*__STATIC_INLINE void disable_SysTick(void) {
@@ -359,14 +364,14 @@ void HAL_delay(__IO uint32_t delay) {
   end = start + delay;
   while( (end < start && (now >= start || now <= end)) || (start <= end && (now >= start && now <= end))) {
     // feed IWDG
-    IWDG_Feed()
-    // sleep
+    IWDG_Feed();
+    // 进入休眠模式
     HAL_PWR_EnterSLEEPMode(PWR_LOWPOWERREGULATOR_ON, PWR_SLEEPENTRY_WFI); // TODO: test
     now = HAL_GetTick();
     if (now == past) {
-      // 如果 now 在 10000 次循环后仍然没有变化
+      // 如果 now 在 20 次循环后仍然没有变化
       // 说明定时器出错
-      if (++count > 10000) Error_Handler(3);
+      if (++count > 20) Error_Handler(3);
     } else {
       past = now, count = 0;
     }
@@ -374,7 +379,7 @@ void HAL_delay(__IO uint32_t delay) {
 }
 
 void loop_delay(int time){
-  for(int i = 0; i < time; i++);
+  while(time--);
 }
 
 /**
@@ -399,9 +404,7 @@ void Error_Handler(int err)
 	} else {
     printf("@ Receive an error code: %d!\r\n", err);
   }
-  while(1) 
-  {
-  }
+  while(1);
   /* USER CODE END Error_Handler */ 
 }
 
