f558cb96913d48843cd520d4846b3a53b836a90b
Qing_LKYi <597572246@qq.com>
2023-05-30 14:12:52 +0800

    fix: change timeout to 0.1s

--- Src/IWDG.c
+++ Src/IWDG.c
@@ -4,8 +4,8 @@ IWDG_HandleTypeDef h_iwdg;
 
 void IWDG_Init(void) {
     h_iwdg.Instance = IWDG;
-    h_iwdg.Init.Prescaler = IWDG_PRESCALER_16;
-    h_iwdg.Init.Reload = 125; // tout = 16 * 125 / 40k = 0.05
+    h_iwdg.Init.Prescaler = IWDG_PRESCALER_32;
+    h_iwdg.Init.Reload = 125; // tout = 32 * 125 / 40k = 0.1
     if(HAL_IWDG_Init(&h_iwdg) != HAL_OK){
         Error_Handler(4);
     }
